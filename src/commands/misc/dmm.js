728ca6f485a4177c1acef6f52a04ebac9d3ad93b
rei666 <h1.tnguyen088@gmail.com>
2024-11-18 15:17:19 +0700

    Fix 'd** function'

--- src/commands/misc/dmm.js
+++ src/commands/misc/dmm.js
@@ -14,26 +14,27 @@ module.exports = {
       description: 'The user to dou.',
       required: true,
       type: ApplicationCommandOptionType.Mentionable,
-    },
-    {
-      name: 'reason',
-      description: 'The reason for banning.',
-      type: ApplicationCommandOptionType.String,
-    },
+    }
   ],
-  permissionsRequired: [PermissionFlagsBits.Administrator],
-  botPermissions: [PermissionFlagsBits.Administrator],
 
   callback: (client, interaction) => {
+    // Retrieve the target-user option
     const targetObject = interaction.options.get('target-user');
+
+    // Ensure the target object and user exist
+    if (!targetObject || !targetObject.user) {
+      return interaction.reply('Could not find the target user!');
+    }
+
+    // Get the guild member
     const member = interaction.guild.members.cache.get(targetObject.user.id);
 
     if (member) {
       // Respond with the member's display name
-      interaction.reply(`Địt mẹ mày ${member.displayName}`);
+      interaction.reply(`Địt mẹ mày ${member}`);
     } else {
       // Respond with the author's username
-      interaction.reply(`Địt mẹ mày, ${member.username}!`);
+      interaction.reply(`Địt mẹ mày, <@${targetObject.user.id}>!`);
     }
   },
 };
